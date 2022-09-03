9e08073f13a288d8180ce87a62fbf4734a91118d
Prasad Dananjaya Wilagama <85976286+PrasadWilagama@users.noreply.github.com>
2022-09-03 15:09:09 +0530

    Add files via upload
    Adding Day 01 R codes For the Referance

---
+++ R_Code_01.R
@@ -0,0 +1,13 @@
+#R programming Series With Prasad D Wilagama 
+#Reach me LinkedIn 
+
+
+
+#printing names in R 
+"Prasad Wilagama"
+"Hello World"
+print("Hello World")
+
+print("My name is Prasad Wilagama")
+
+
