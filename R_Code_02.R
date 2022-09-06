7d854a6651deb00c7a6c521f3d6f9e02bc71314f
Prasad Dananjaya Wilagama <85976286+PrasadWilagama@users.noreply.github.com>
2022-09-06 12:11:43 +0530

    Add files via upload
    R day 2 codes

---
+++ R_Code_02.R
@@ -0,0 +1,35 @@
+#R programming Series With Prasad D Wilagama 
+#Reach me LinkedIn 
+
+# Assign numeric values to the variables
+a = 1 
+b = 2
+c = 3
+#
+# Display values 
+a
+b
+c
+#
+# View values using print()
+print(a)
+print(b)
+print(c)
+#
+# Assign a new value to a
+a = 10
+#
+# Add variables and display result
+a = a + b + c
+print(a)
+#
+# Other assignment operators
+P = 25
+Q= 50 
+#
+# Display Sum of P and Q
+print(P + Q)
+#
+# Assign text to a variable
+R= "Hello World,R with Prasad Wilagama"
+print(R)
