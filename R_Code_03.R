833061120d885f8ad2a5e4d678685a2249a94271
Prasad Dananjaya Wilagama <85976286+PrasadWilagama@users.noreply.github.com>
2022-09-07 09:19:26 +0530

    Add files via upload
    Add Day 3 Code to the Repo

---
+++ R_Code_03.R
@@ -0,0 +1,25 @@
+#R programming Series With Prasad D Wilagama 
+#Reach me LinkedIn 
+
+# Create some variables
+a = 1 
+b = 2
+c = 3
+#
+# Perform Calculation
+total = a + b + c
+print(total)   # Display result
+
+# Comment useful for complicated code
+#
+# Calculate Body Mass Index (BMI)
+Height =  1.62  # metres
+Weight = 45.3   # kilograms
+#
+
+Height
+Weight
+
+# formula
+BMI <- Weight/(Height * Height)
+print(BMI)
