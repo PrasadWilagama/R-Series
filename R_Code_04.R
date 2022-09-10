cc0c55e377688f8a087570202a8550ae6cdb7aff
Prasad Dananjaya Wilagama <85976286+PrasadWilagama@users.noreply.github.com>
2022-09-10 19:08:29 +0530

    Add files via upload
    Adding Day 4 Codes to the repo

---
+++ R_Code_04.R
@@ -0,0 +1,25 @@
+#R programming Series With Prasad D Wilagama 
+#Reach me LinkedIn 
+
+
+# Create a simple vector using combine function
+v1 = c(2, 4, 6)
+print(v1)
+#
+v2 = c(1, 3, 5)
+print(v2)
+#
+# Add two vectors
+v1 + v2
+tot=v1+v2
+tot
+#
+# combine two vectors
+c(v1, v2)  
+#
+# Create a vector with text
+v3 = c("R", "is", "Cool")
+print(v3)
+#
+# Try to combine the Vectors
+c(v1, v2, v3)
