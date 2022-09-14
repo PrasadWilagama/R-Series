aacd0f88b61d10f48927b0065f40c1ff59654045
Prasad Dananjaya Wilagama <85976286+PrasadWilagama@users.noreply.github.com>
2022-09-14 12:50:10 +0530

    Add files via upload
    Adding Day 5 R Codes to the Repo

---
+++ R_Code_05.R
@@ -0,0 +1,10 @@
+#R programming Series With Prasad D Wilagama 
+#Reach me LinkedIn 
+
+# Read in Data
+Gas = c(30, 23, 14, 25, 05, 02, 13, 11, 27, 29, 19, 31)
+print(Gas)
+
+Gram = c(5, 8, 12, 19, 23, 27, 30, 33, 37, 40, 44, 51)
+print(Gram)
+
