84550f0ce1d56cac989e6e9adcc13d2a01ff55e9
Prasad Dananjaya Wilagama <85976286+PrasadWilagama@users.noreply.github.com>
2022-09-16 12:52:17 +0530

    Add files via upload
    Adding Day 6 Code to the  Repo

---
+++ R_Code_06.R
@@ -0,0 +1,23 @@
+#R programming Series With Prasad D Wilagama 
+#Reach me LinkedIn 
+
+# Read in Sample Data
+
+Gas = c(30, 23, 14, 25, 05, 02, 13, 11, 27, 29, 19, 31)
+
+Gram = c(5, 8, 12, 19, 23, 27, 30, 33, 37, 40, 44, 51)
+
+# Calculate the Mean, Median, Standard Deviation, 
+# Variance, and Correlation Coefficient of each vector
+#
+mean (Gas)
+median (Gas) 
+sd (Gas)
+var (Gas)
+cor (Gas, Gram)   # Default is Pearson's Correlation Coefficient
+#
+mean (Gram)
+median (Gram)
+sd (Gram)
+var (Gram)
+cor (Gram, Gas)   
